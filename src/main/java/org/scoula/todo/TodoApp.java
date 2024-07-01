3c0eeef54b74bada54cc11692c26f5bd81404633
Kim Joon Kyoung <gang_3@naver.com>
2024-07-01 14:10:49 +0900

    TodoApp v2

--- src/main/java/org/scoula/todo/TodoApp.java
+++ src/main/java/org/scoula/todo/TodoApp.java
@@ -13,6 +13,12 @@ public class TodoApp extends App {
     public void createMenu(Menu menu) {
         super.createMenu(menu);
 
+        /*menu.add(new MenuItem("목록", new PrintTodoCommand()));
+        menu.add(new MenuItem("상세", new DetailTodoCommand()));
+        menu.add(new MenuItem("추가", new AddTodoCommand()));
+        menu.add(new MenuItem("수정", new UpdateTodoCommand()));
+        menu.add(new MenuItem("삭제", new DeleteTodoCommand()));*/
+
         menu.add(new MenuItem("목록", service::printTodoList));
         menu.add(new MenuItem("상세", service::detailTodo));
         menu.add(new MenuItem("추가", service::addTodo));
